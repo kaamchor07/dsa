052d48367ed1e0e8cf1eb5f389fe06386b2ca6e0
HarshSolanki <na24b036@smail.iitm.ac.in>
2026-01-19 09:45:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (79.05%)

---
+++ solutions/0075-sort-colors/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        int k=0;
+        int j=n-1;
+        while(k<=j){
+            if(nums[k]==0){
+                swap(nums[k],nums[i]);
+                k++;
+                i++;
+            }else if(nums[k]==2){
+                swap(nums[k],nums[j]);
+                j--;
+            }else{
+                k++;
+            }
+        }
+    }
+};
