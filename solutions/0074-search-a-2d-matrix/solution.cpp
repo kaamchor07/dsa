293f4701e539debfcb68bf19a733a7060c03b4b5
HarshSolanki <na24b036@smail.iitm.ac.in>
2026-02-10 18:00:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (45.10%)

---
+++ solutions/0074-search-a-2d-matrix/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int mid;
+        int low=0;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int high=(m*n)-1;
+        while(high>=low){
+            mid=low+(high-low)/2;
+            if(target>matrix[mid/n][mid%n]){
+                low=mid+1;
+            }else if(target==matrix[mid/n][mid%n]){
+                return true;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
