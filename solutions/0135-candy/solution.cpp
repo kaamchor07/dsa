0d170b41e8211de08abea966d978984fb471b554
HarshSolanki <na24b036@smail.iitm.ac.in>
2026-06-06 09:29:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23 MB (52.09%)

---
+++ solutions/0135-candy/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n=nums.size();
+        int a=0;
+        vector<int>ans(n,1);
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i-1]) {
+                ans[i] = ans[i-1] + 1;
+            }
+        }
+
+        // 2. Right-to-left pass
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] > nums[i+1]) {
+                ans[i] = max(ans[i], ans[i+1] + 1);
+            }
+        }
+        for(const auto& i : ans){
+            a+=i;
+        }
+        return a;
+    }
+};
