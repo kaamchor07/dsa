393dd659775e81041804b19411763d6917a4b21f
HarshSolanki <na24b036@smail.iitm.ac.in>
2026-07-06 14:19:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11 MB (73.44%)

---
+++ solutions/0145-binary-tree-postorder-traversal/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void postorder(TreeNode* node,vector<int>&ans){
+        if(node==nullptr){
+            return;
+        }
+        postorder(node->left,ans);
+        postorder(node->right,ans);
+        ans.push_back(node->val);
+        
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        postorder(root,ans);
+        return ans;
+    }
+};
