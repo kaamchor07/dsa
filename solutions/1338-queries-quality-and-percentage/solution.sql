bd29b096a42567299b7dece31156d6da0e71a85b
HarshSolanki <na24b036@smail.iitm.ac.in>
2026-07-23 19:20:33 +0000

    Sync LeetCode submission Runtime - 394 ms (35.42%), Memory - 0.0B (100.00%)

---
+++ solutions/1338-queries-quality-and-percentage/solution.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+SELECT query_name, 
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND(SUM(rating<3)*100/COUNT(*),2) 
+    AS poor_query_percentage
+FROM 
+    Queries
+WHERE
+    query_name is not null
+GROUP BY   
+    query_name;
+
+
