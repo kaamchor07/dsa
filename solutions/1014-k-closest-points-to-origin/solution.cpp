fd2ce0d54bd6e7e84055722411a0619926b4054d
HarshSolanki <na24b036@smail.iitm.ac.in>
2026-07-28 13:19:30 +0000

    Sync LeetCode submission Runtime - 36 ms (66.16%), Memory - 66.8 MB (83.51%)

---
+++ solutions/1014-k-closest-points-to-origin/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,less<pair<int,int>>>pq;
+        for(int i=0;i<points.size();i++){
+            int dist=pow(points[i][0],2)+pow(points[i][1],2);
+            if(pq.size()<k){
+                pq.push({dist,i});
+            }else{
+                if(pq.top().first>dist){
+                    pq.pop();
+                    pq.push({dist,i});
+                }
+            }
+        }
+        vector<vector<int>>ans;
+        while(!pq.empty()){
+            int index=pq.top().second;
+            pq.pop();
+            ans.push_back(points[index]);
+        }
+        return ans;
+    }
+};
